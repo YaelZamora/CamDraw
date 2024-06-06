67f19b0e1955df9f674a1a8e2ddbde27fe0602e7
YaelZamora <ajedrezkasparov@gmail.com>
2024-06-06 08:50:54 -0600

    Initial Commit

---
+++ CamDraw/CamDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamDrawApp.swift
+//  CamDraw
+//
+//  Created by Yael Javier Zamora Moreno on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CamDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
